6c64ac60b4bf2f16a7292226a7c8043819d01c81
nerucha14 <66110700@dpu.ac.th>
2024-02-14 12:13:49 +0700

    forecast

---
+++ weathercast/lib/forecast.dart
@@ -0,0 +1,26 @@
+import 'package:geolocator/geolocator.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:http/http.dart' as http;
+import 'location.dart';
+import 'weather.dart';
+
+Future<Weather> forecast() async {
+  const url = 'https://data.tmd.go.th/nwpapi/v1/forecast/location/hourly/at';
+  const token =
+      'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImU2ZmJhZmYyYjFlYjBjZTY4MDE2NmYwZjgxODk1MDYyODZkOGY1NmUwYmQ1ZGFkNzgxNTMzZWVkMjY2ZDVhYjcxMGI5YWFlN2I1MzUxMjUxIn0.eyJhdWQiOiIyIiwianRpIjoiZTZmYmFmZjJiMWViMGNlNjgwMTY2ZjBmODE4OTUwNjI4NmQ4ZjU2ZTBiZDVkYWQ3ODE1MzNlZWQyNjZkNWFiNzEwYjlhYWU3YjUzNTEyNTEiLCJpYXQiOjE3MDc4ODIyNTksIm5iZiI6MTcwNzg4MjI1OSwiZXhwIjoxNzM5NTA0NjU5LCJzdWIiOiIzMDAyIiwic2NvcGVzIjpbXX0.S29sMOWPcBsJW81T9HpQXNFiFTuFJEg2m9lNhxJyejWZdKKR9MiVMJ4x68h_G0Jijdri8GdDPLotFnySxE9QAnjcIUUY6v-q9VdoFmJRUGqgSkagAVcMFdqxRHjrlZ6K6UV5kinHfes6yAU8sVLpZ3gRx-ThMwhEHIuvIbzQbpK2aiY7IUjo8zFKF15cxgs3c8UdHTAs9Xvcy_MhhfBoiI8AEscPbLnHXNV2Ve2-epywrOeG3RX83sWDib-xAcgCtZCOqHZOYs0hHjcErVVkdmD5Q0bedrIaHbypHQS3PhOR3wjQi1VOmj1KdRSqZFsZQVMGi4dGJdgZZAWir8bNRsVvClc1xKQS3maqdXGnsi9IcGF89AiDmgGdkwGiv0uNqtYvyh6WINsfXQtFWwTKu8AJVS7bDEzHXeRMklEM_0pHyvJFxjDkdQeekPLslcrIb6PNap0Tw5IW3AtToWyE3RHlQLel32IGJmBHBNsW90KPfh-PiPK1vk1nPD-ypzwcFT4O_0nk3980kfb5dqWU_miqRsgFAdcyJmQpVSTNS0SKBBcV69S356jFiYDSxvZpvivmspnfxb5uFe1d64ittcO6bRKDO1mdK2jFy94Bf4R23GTvQ_4P2ukhOGYYeNZcp9q70zbmBa0mm4zq6aMz-BWdlsSj99eM-L1M-eVtqUU';
+
+  try {
+    Position location = await getCurrentLocation();
+    http.Response response = await http.get(
+      Uri.parse(
+          '$url?lat=${location.latitude}&lon=${location.longitude}&fields=tc,cond'),
+      headers: {
+        'accept': 'application/json',
+        'authorization': 'Bearer $token',
+      },
+    );
+    return Weather(address: response.body, temperature: 0, cond: 0);
+  } catch (e) {
+    return Future.error(e);
+  }
+}
--- weathercast/lib/report.dart
+++ weathercast/lib/report.dart
@@ -1,5 +1,9 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
 import 'location.dart';
+import 'forecast.dart';
 
 class Report extends StatefulWidget {
   const Report({super.key});
@@ -11,10 +15,14 @@ class Report extends StatefulWidget {
 class _ReportState extends State<Report> {
   @override
   void initState() {
-    getCurrentLocation().then((value) => print(value));
+    forecast().then((v) => print(v.address));
+    //getCurrentLocation().then((location) =>
+    //    placemarkFromCoordinates(location.latitude, location.longitude)
+    //        .then((Placemarks) => print(Placemarks.first)));
     super.initState();
   }
 
+  @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
