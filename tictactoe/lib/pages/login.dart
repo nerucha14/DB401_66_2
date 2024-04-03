b21ae4096888007814448318e3e3e8b4e33794a5
nerucha14 <66110700@dpu.ac.th>
2024-04-03 12:15:34 +0700

    register

--- tictactoe/lib/pages/login.dart
+++ tictactoe/lib/pages/login.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:tictactoe/firebase_options.dart';
@@ -10,16 +11,113 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  late String _playername;
+  late String _password;
+  bool _ready = false;
+
   @override
   void initState() {
     Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform)
-        .whenComplete(() => print('firebase connected'));
+        .whenComplete(() {
+      setState(() {
+        _ready = true;
+      });
+    });
     super.initState();
   }
 
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('login'),
+    void gotoChallenge() {
+      Navigator.pushReplacementNamed(context, 'challenge');
+    }
+
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Image.asset(
+                'images/logo.png',
+                height: 100,
+              ),
+              const SizedBox(
+                height: 48,
+              ),
+              TextField(
+                onChanged: (value) {
+                  _playername = value;
+                },
+                keyboardType: TextInputType.name,
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                    hintText: 'Enter your player name',
+                    border: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                            color: Colors.blueAccent, width: 1),
+                        borderRadius: BorderRadius.circular(30)),
+                    contentPadding: const EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 20)),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              TextField(
+                onChanged: (value) {
+                  _password = value;
+                },
+                obscureText: true,
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                  hintText: 'Enter your password',
+                  border: OutlineInputBorder(
+                      borderSide:
+                          const BorderSide(color: Colors.blueAccent, width: 1),
+                      borderRadius: BorderRadius.circular(30)),
+                  contentPadding:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                ),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              FilledButton(
+                  onPressed: _ready
+                      ? () async {
+                          try {
+                            await FirebaseAuth.instance
+                                .signInWithEmailAndPassword(
+                                    email: '$_playername@gmail.com',
+                                    password: _password);
+                            gotoChallenge();
+                          } on FirebaseAuthException catch (e) {
+                            if (!context.mounted) return;
+                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                              content: Text(e.code),
+                              duration: const Duration(seconds: 10),
+                            ));
+                          } catch (e) {
+                            if (!context.mounted) return;
+                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                              content: Text(e.toString()),
+                              duration: const Duration(seconds: 10),
+                            ));
+                          }
+                        }
+                      : null,
+                  child: const Text('login')),
+              const SizedBox(height: 8),
+              TextButton(
+                onPressed: _ready
+                    ? () => Navigator.pushNamed(context, 'register')
+                    : null,
+                child: const Text('New Player Click Here!'),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- tictactoe/lib/pages/register.dart
+++ tictactoe/lib/pages/register.dart
@@ -8,10 +8,88 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  late String _playername;
+  late String _password;
+  late String _passwordCheck;
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('register'),
+    void gotoChallenge() {
+      Navigator.pushNamedAndRemoveUntil(context, 'challenge', (route) => false);
+    }
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('New Player')),
+      body: Center(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Image.asset('images/logo.png', height: 100),
+              const SizedBox(
+                height: 48,
+              ),
+              TextField(
+                onChanged: (value) {
+                  _playername = value;
+                },
+                keyboardType: TextInputType.name,
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                    hintText: 'Enter your player name',
+                    border: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                            color: Colors.blueAccent, width: 1),
+                        borderRadius: BorderRadius.circular(30)),
+                    contentPadding: const EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 20)),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              TextField(
+                onChanged: (value) {
+                  _password = value;
+                },
+                obscureText: true,
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                  hintText: 'Enter your password',
+                  border: OutlineInputBorder(
+                      borderSide:
+                          const BorderSide(color: Colors.blueAccent, width: 1),
+                      borderRadius: BorderRadius.circular(30)),
+                  contentPadding:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                ),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              TextField(
+                onChanged: (value) {
+                  _passwordCheck = value;
+                },
+                obscureText: true,
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                  hintText: 'Retype your password',
+                  border: OutlineInputBorder(
+                      borderSide:
+                          const BorderSide(color: Colors.blueAccent, width: 1),
+                      borderRadius: BorderRadius.circular(30)),
+                  contentPadding:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                ),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              FilledButton(onPressed: () {}, child: const Text('Register')),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
